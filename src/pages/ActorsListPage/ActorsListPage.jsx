3f91859bba482f23580e0224f43270b617cc7fa3
akyeshi <akyeshi@outlook.com>
2024-05-07 10:50:06 -0400

    react movies lab - part 3 done

--- src/pages/ActorsListPage/ActorsListPage.jsx
+++ src/pages/ActorsListPage/ActorsListPage.jsx
@@ -1,8 +1,9 @@
 import ActorCard from "../../components/ActorCard/ActorCard";
 
 
-export default function ActorsListPage({movies}) {
+export default function ActorsListPage({ movies }) {
 
+  /*
   let obj = {}; 
   movies.forEach(movie => {
     movie.cast.forEach(actor => {
@@ -11,10 +12,21 @@ export default function ActorsListPage({movies}) {
   }); 
 
   let actorList = Object.keys(obj); 
+  */
+
+  let allActorsList = [];
+  movies.forEach(movie => {
+    movie.cast.forEach(actor => {
+      allActorsList.push(actor);
+    })
+  });
+  // new Set returns {1, 2, 3} => Array.from(new Set) => [1, 2, 3]
+  console.log(new Set(allActorsList));
+  const uniqueActorList = Array.from(new Set(allActorsList));
 
   return (
     <div className="container">
-      {actorList.map(actor => {
+      {uniqueActorList.map(actor => {
         return <ActorCard key={actor} actor={actor} />
       })}
     </div>
